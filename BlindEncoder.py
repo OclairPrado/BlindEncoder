dcd1d0975ea64916850f742f0c62d25f767998d2
OclairPrado <55806109+OclairPrado@users.noreply.github.com>
2022-05-05 15:11:52 -0300

    Update BlindEncoder.py
    Ajustes de performance por Daniel Credico de Coimbra

--- BlindEncoder.py
+++ BlindEncoder.py
@@ -1,80 +1,72 @@
+# NOVA VERSÃO DANIEL
 class BlindEncoder:
-  categories_ = {}
-  
+  categories_ = dict()
+    
+    
   def fit(self, df):
     if df is None or len(df) == 0:
-      raise ValueError('empty dataframe')
-    # Agrupa todos os elementos
+      raise ValueError('Empty DataFrame.')
 
-    # Passa df para maiúsculas
-    df = pd.concat([df[col].astype(str).str.upper() for col in df.columns], axis=1)
+    df = pd.concat([df[col].map(lambda x: x.upper() if isinstance(x, str) else x) for col in df.columns], axis=1)
 
-    elementos = []
-    for coluna in df:
-      for elemento in df[coluna]:
-        elementos.append(elemento)
-    # Remove duplicidades
-    categ_list = np.unique(elementos, return_index=False, return_inverse=False, return_counts=False, axis=None)
+    cat_list = []
+    for col in df.columns:
+      cat_list.extend(df[col].unique())
+    cat_list = set(cat_list)
 
-    # Insere os valores para as categorias
+    num_set = set()
+    for cat in cat_list:
+      if isinstance(cat, (int, float, np.int64, np.float64)):
+        num_set.add(cat)
+
     self.categories_ = {}
-    pos = 0
-    for categ in categ_list:
-      self.categories_[categ] = pos
-      pos = pos + 1
-    # Garante que o encoder tenha referencia para 'DESCONHECIDO'
-    if not 'DESCONHECIDO' in self.categories_:
-        self.categories_['DESCONHECIDO'] = len(self.categories_) + 1
+    num_cats     = len(cat_list)
+    code         = 0
+    for cat in cat_list:
+      if isinstance(cat, (int, float, np.int64, np.float64)):
+        self.categories_[cat] = cat
+      else:
+        code = min( set(range(code, num_cats)) - num_set )
+        self.categories_[cat] = code
+        code += 1
+        
+    self.categories_['DESCONHECIDO'] = self.categories_.get('DESCONHECIDO', num_cats+1)
+    
+
 
   def transform(self, df):
     if df is None or len(df) == 0:
-      raise ValueError('empty dataframe')
+      raise ValueError('Empty DataFrame.')
+
+    if len(self.categories_.keys()) == 0:
+      raise ValueError('Probably missing .fit().')
+
+    df = pd.concat([df[col].map(lambda x: x.upper() if isinstance(x, str) else x) for col in df.columns], axis=1)
+
+    cat_list = []
+    for col in df.columns:
+      cat_list.extend(df[col].unique())
+    cat_list = set(cat_list)
+
+    for cat in cat_list:
+      self.categories_[cat] = self.categories_.get(cat, self.categories_['DESCONHECIDO'])
+  
+    df_enc = df.apply(lambda col: col.map(self.categories_))
+
+    return df_enc
+
+
+
+  def inverse_transform(self, df_enc):
+    if df_enc is None or len(df_enc) == 0:
+      raise ValueError('Empty DataFrame.')
+
     if len(self.categories_) == 0:
-      raise ValueError('probably missing fit()')
-    
-    # Passa df para maiúsculas
-    df = pd.concat([df[col].astype(str).str.upper() for col in df.columns], axis=1)
+      raise ValueError('Probably missing .fit().')
 
-    # Garante que o encoder tenha referencia para 'DESCONHECIDO'
-    if not 'DESCONHECIDO' in self.categories_:
-        self.categories_['DESCONHECIDO'] = len(self.categories_) + 1
-
-    # Agrupa todos os elementos do novo dataframe
-    elementos = []
-    for coluna in df:
-      for elemento in df[coluna]:
-        elementos.append(elemento)
-    # Remove duplicidades
-    categ_list = np.unique(elementos, return_index=False, return_inverse=False, return_counts=False, axis=None)
-
-    # Troca as categorias conhecidas pelos números. As categorias desconhecidas ganham o número equivalene ao 'desconhecido'
-    #Obs.: altera apenas string
-    df = df.copy()
-    for categ in categ_list:
-      if isinstance(categ, str):
-        if categ in self.categories_:
-          df = pd.DataFrame(np.where(df == categ, self.categories_[categ], df))
-        else:
-          df = pd.DataFrame(np.where(df == categ, self.categories_['DESCONHECIDO'], df))
+    categories_inv = {v:k for k,v in self.categories_.items()}
+    categories_inv[self.categories_['DESCONHECIDO']] = 'DESCONHECIDO'
+    df = df_enc.apply(lambda col: col.map(categories_inv))
 
     return df
-
-  def inverse_transform(self, df_rev):
-    if df_rev is None or len(df_rev) == 0:
-      raise ValueError('empty dataframe')
-    if len(self.categories_) == 0:
-      raise ValueError('probably missing fit()')
-    
-    res = {}
-    dict_categ = self.categories_ 
-    chaves = list(dict_categ.keys())
-    valores = list(dict_categ.values())
- 
-    for lin in range(df_rev.shape[1]):
-      for col in range(df_rev.shape[0]):
-        if df_rev[lin][col] in valores:
-          pos = valores.index( df_rev[lin][col] )
-          res[pos] = chaves[pos]
-
-    return res
-    
+  
